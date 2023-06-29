2c9ff44f3141aff28b3835e5f202cf17ea6437b1
Marina <PyatkovaM@yandex.ru>
2023-06-30 00:37:39 +0300

    решение 38 задачи

---
+++ 38/Program.cs
@@ -0,0 +1,71 @@
+﻿//  Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Clear();
+
+// Метод для создания массива
+
+double[] array = GetArray (10, 10, 100);
+Console.WriteLine (String.Join (" ", array));
+
+// Метод для нахождения наибольшего значения
+double max = MaxValue(array);
+Console.WriteLine ($" {max} ");
+
+// Метод для нахождения наибольшего значения
+double min = MinValue(array);
+Console.WriteLine ($" {min} ");
+
+//Вывод результата
+Console.WriteLine ($" {max - min} ");
+
+
+
+// Создаем массив
+double[] GetArray(int size, double minValue, double maxValue)
+{
+    double[] res = new double [size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res [i] = new Random().NextDouble();
+    }
+    return res;
+}
+
+
+//находим наибольшее значение
+double MaxValue(double[] arr)
+
+{
+     double[] resultMax = arr[i]]
+
+    for (int i = 0; i < arr.Length; i++,)
+    {
+        if(arr[i] < resultMax)
+        {
+            resultMax = arr[i];
+        }
+    }
+    return resultMax;
+}
+
+
+//находим наименьшее значение
+double MinValue(double[] arr)
+
+{
+     double[] resultMin = arr[i]
+
+    for (int i = 0; i > arr.Length; i++,)
+    {
+        if(arr[i] > resultMin)
+        {
+            resultMin = arr[i];
+        }
+    }
+    return resultMin;
+}
+
+
+
+
